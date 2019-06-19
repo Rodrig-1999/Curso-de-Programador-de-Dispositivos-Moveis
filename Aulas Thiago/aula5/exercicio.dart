ec32e3b176c20ef6ab0309332e057e1389ebaecc
Rodrigo Paulo dos Santos <49247264+Rodrig-1999@users.noreply.github.com>
2019-06-19 17:16:41 -0300

    Add files via upload

---
+++ Aulas Thiago/aula5/exercicio.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+//void main() => runApp(MyApp());
+
+//Método Principal, que é a mesma coisa do de cima, porém é aberta com chaves ao invés de seta
+void main() {
+  runApp(MyApp());
+  //Instanciar a Classe Pessoa (como se fosse uma cópia)
+  Pessoa pessoa1 = new Pessoa();
+  pessoa1.nome = "Andy";
+  pessoa1.curso = "Prog. Disp. Móveis";
+  pessoa1.identidade_genero = "Neutro";
+  pessoa1.ano_nasc = 2002;
+//Chamada da função/método que será executado
+  pessoa1.mostrar_idade()
+}
+
+
+//Classe Pessoa
+//Classe é um modelo para um objeto, a planta na construção da casa, a forma do bolo, etc
+class Pessoa{
+//Atributos da classe, as características ou informações que o objeto irá armazenar
+  String nome;
+  String curso;
+  String identidade_genero;
+  int idade;
+  int ano_nasc;
+
+  void mostrar(){
+    print("O indivíduo $nome realizou uma matrícula no curso $curso");
+  }
+
+  void calcular_idade(){
+    idade = 2019 - ano_nasc;
+
+    if(identidade_genero == "Masculino") {
+      print("O $nome nasceu em $ano_nasc e tem $idade anos");
+    }else if(identidade_genero == "Feminino") {
+      print("A $nome nasceu em $ano_nasc e tem $idade anos");
+    }else if(identidade_genero == "Neutro"){
+      print("$nome nasceu em $ano_nasc e tem $idade anos");
+    }
+  }
+
+  int mostrar_idade(int ano_nasc){
+    // o nome "age" foi para não confundirmos ela com a lá de cima
+    int age = 2019 - ano_nasc;
+    bool maior;
+
+    if(age >= 18){
+      maior = true;
+    }
+    else{
+      maior = false;
+    }
+  }
+}
+
+//Classe Myapp
+class MyApp extends StatefulWidget{
+    @override
+  State<StatefulWidget> createState(){
+    return _MyappState();
+  }
+}
+
+//Classe MyappState que herda (extends) a classe State, que também é um widget. O underscore é usado porque a classe privada
+class _MyappState extends State<MyApp>
+{
+
+//Interface #################################################
+@override
+Widget build(BuildContext context)
+{
+  return MaterialApp(
+      title: "Exemplo de Classes e Objetos",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Classes e Objetos"),
+          ),
+        ),
+      ),
+    );
+  }
+}
